d0f4e0994974e0741034eef1b58f6a8fd81827e5
pratiksutar841 <pratiksutar841@gmail.com>
2024-01-06 20:24:25 +0530

    Varibles

---
+++ program2.py
@@ -0,0 +1,19 @@
+#Variales
+
+# Assigning values to variables
+x = 90
+name = "Alice"
+pi = 3.98
+
+
+#    int x = 5; But in python we need not do that.
+x = 10 # Integer
+name = "Alice" # String
+pi = .14 # Float
+is_stude5t = True # Boolean
+
+
+x = 5
+print(x) # Output: 5
+x = "Hello"
+print(x) # Output: Hell
