2234feed3a78cc0d5e72f3327841ffd72c848939
pratiksutar841 <pratiksutar841@gmail.com>
2024-01-06 21:45:14 +0530

    Volume-sphere

---
+++ program6.py
@@ -0,0 +1,8 @@
+# Taking input from the user
+radius = float(input("Enter the radius of the sphere: "))
+ 
+# Calculating the volume of the sphere
+volume = (4/3) * 3.14159 * (radius ** 3)
+ 
+# Displaying the output
+print("\nThe Volume of the Sphere is:", volume)
