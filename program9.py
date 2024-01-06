2c0c0cd1ed4d511fd9496c73989a5ae80b04a24f
pratiksutar841 <pratiksutar841@gmail.com>
2024-01-06 21:56:44 +0530

    typecasting

---
+++ program9.py
@@ -0,0 +1,14 @@
+#  A. Integer to String
+num = 42
+num_str = str(num)
+print(num_str, type(num_str))
+
+#  B. String to Integer
+num_str = "123"
+num = int(num_str)
+print(num, type(num))
+
+#  C. Float to Integer
+float_num = 3.14
+integer_num = int(float_num)
+print(integer_num, type(integer_num))
