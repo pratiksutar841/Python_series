65fe8b2f4dfa51acd5cd523625fa5c94153a5876
pratiksutar841 <pratiksutar841@gmail.com>
2024-01-06 20:16:02 +0530

    Hello-Word

--- program1.py
+++ program1.py
@@ -4,3 +4,14 @@ print("Hello,\nWorld!")
 
 print("Hello,",end="`")
 print("World!")
+
+# This is a single-line comment
+x = 5 # Assigning a value to x
+
+# Multi-Line Comments
+
+'''
+This is a multi-line comment or docstring.
+It can span multiple lines and is commonly used to provide
+detailed explanations about code sections or documentation.
+'''
