2f1356f95a258d0d6fcd954d40d74830c428ca6d
pratiksutar841 <pratiksutar841@gmail.com>
2024-01-07 13:22:52 +0530

    Control-Statement1

---
+++ program10.py
@@ -0,0 +1,11 @@
+#Take an integer input and tell if it is positive or negative (zero not included)
+# Taking integer input from the user
+number = int(input("Enter an integer: "))
+ 
+# Checking if the number is positive or negative
+if number > 0:
+    print("The number is positive.")
+else:
+    print("The number is negative.")
+
+
