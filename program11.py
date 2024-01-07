f4c9dbaf21c82136e2913fcdcafe1d20537ac385
pratiksutar841 <pratiksutar841@gmail.com>
2024-01-07 13:24:54 +0530

    Control-Statement2

---
+++ program11.py
@@ -0,0 +1,8 @@
+# Taking positive integer input from the user
+number = int(input("Enter a positive integer: "))
+ 
+# Checking if the number is even or odd
+if number % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
