631de011f9d0ee94e2c3324aaeaafc97adb505b7
pratiksutar841 <pratiksutar841@gmail.com>
2024-01-08 22:34:32 +0530

    While-Loop

---
+++ program19.py
@@ -0,0 +1,22 @@
+'''
+while condition:
+# Code block to execute
+'''
+'''===================================================='''
+# Question: Print numbers from 1 to 100
+
+num = 1
+while num <= 100:
+    print(num)
+    num += 1
+
+'''====================================================='''
+# Question: Print even numbers from 1 to 100
+num = 1
+ 
+while num <= 100:
+    if num % 2 == 0:
+        print(num)
+    num += 1
+
+'''====================================================='''
